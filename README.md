8f2c5d38d2bee1d742999ed008ffc1b5ce57bbb2
Jian Tay <jian.tay@colorado.edu>
2022-05-18 11:29:15 -0600

    v0.1
    * Changed requirements from ND2 reader to Bioformats to simplify installation and interoperability
    * Moved parameters to variables defined near top of script
    * Changed visualization of cells with puncta for easier identification

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-Cell counting
+# RNAscope quantification
 
-Preliminary code to count cells.
+Welcome to the RNAscope quantification project! This code is being 
+developed in collaboration with Connor McNulty and the Baratta lab at CU 
+Boulder.
+
+This project has two goals:
+1. Identify GFP labeled cells
+2. Identify and count the number of cells with RNA labeling.
+
+## Installation and Usage
+
+### Required MATLAB toolboxes
+
+This project requires the following MATLAB toolboxes to be installed:
+* Image Processing Toolbox
+
+### Additional toolboxes
+
+This project also requires the following custom toolboxes:
+* [ND2SDK Matlab toolbox](https://github.com/jwtay1/nd2sdk-MATLAB)
+  
+  This toolbox is required to read in ND2 files into MATLAB. The 
+
+
+
+1. **Download** the code by clicking on the blue Code button above, then 
+   selecting "Download ZIP".
+2. **Extract** the ZIP file into a directory accessible by MATLAB (e.g.
+   in Documents/MATLAB). Navigate to this folder in MATLAB.
+3. **Add the processing folder to the MATLAB path** by right-clicking on
+   the ``processing`` folder, then select "Add to Path" > "Selected Folders
+   and Subfolders".
+
--- countCells.m
+++ countCells.m
@@ -1,24 +1,41 @@
-%Counts number of cells
-
+%RNAscope quantification
 clearvars
 clc
 
+%Parameters
 file = 'D:\Projects\ALMC Tickets\T362-McNulty\data\1c_PLVGATD2D1.nd2';
 
-nd2 = ND2reader(file);
+nuclThreshold = 150;  %Greyscale to threshold nuclei in GFP channel
+maxNuclSize = 50;  %Largest valid object in pixels
+
+%Threshold to count puncta by maximum brightness in the cell
+ch570Threshold = 800;
+ch650Threshold = 3000;
+
+%% Begin processing
+
+%Read in nd2 file
+nd2 = BioformatsImage(file);
 
 %Compute the maximum intensity projection of each channel
-mip = getImage(nd2, 1, 1);
-for iZ = 2:nd2.sizeZ
+for iZ = 1:nd2.sizeZ
 
-    currImage = getImage(nd2, iZ, 1);
-    mip = max(mip, currImage);
+    currImage = zeros(nd2.height, nd2.width, nd2.sizeC, 'uint16');
+
+    for iC = 1:nd2.sizeC
+        currImage(:, :, iC) = getPlane(nd2, iZ, iC, 1);
+    end
+
+    if iZ == 1
+        mip = currImage;
+    else 
+        mip = max(mip, currImage);
+    end
 
 end
 
-%%
-%Make a mask
-nuclMask = mip(:, :, 2) > 150;
+%Make a mask using the GFP channel
+nuclMask = mip(:, :, 2) > nuclThreshold;
 nuclMask = imopen(nuclMask, strel('disk', 1));
 
 dd = -bwdist(~nuclMask);
@@ -28,15 +45,17 @@ dd = imhmin(dd, 0.5);
 L = watershed(dd);
 
 nuclMask(L == 0) = false;
-nuclMask = bwareaopen(nuclMask, 50);
+nuclMask = bwareaopen(nuclMask, maxNuclSize);
 
 %Grow mask
 %nuclMask = imdilate(nuclMask, [1 1; 1 1]);
 
+%Display an image to valide the mask
+figure(1)
 showoverlay(mip(:, :, 2), bwperim(nuclMask))
+title('GFP channel')
 
 %% Count number of cells
-
 for iC = 1:3
 
     %Calculate intensity in each channel
@@ -47,33 +66,65 @@ for iC = 1:3
         validCells{iC} = true(1, numel(chCellData{iC}));
 
     elseif iC == 2
-        validCells{iC} = [chCellData{iC}.MaxIntensity] > 800;
+        validCells{iC} = [chCellData{iC}.MaxIntensity] > ch570Threshold;
 
     elseif iC == 3
-        validCells{iC} = [chCellData{iC}.MaxIntensity] > 3000;
+        validCells{iC} = [chCellData{iC}.MaxIntensity] > ch650Threshold;
 
     end
 
 end
 
-%% Analyze resulting data
+%% Count number of cells
 
-%Count number of cells
+%Number of GFP labeled cells
 numVGaT = nnz(validCells{1})
 
+%Number of GFP labeled cells with puncta ONLY in 520 channel
 numVGaTplusDrd2 = nnz(validCells{1} & validCells{2} & ~validCells{3})
+
+%Number of GFP labeled cells with puncta ONLY in 650 channel
 numVGaTplusDrd1 = nnz(validCells{1} & validCells{3} & ~validCells{2})
+
+%Number of GFP labeled cells with puncta in BOTH in 520 and 650 channels
 numVGaTplusDrd1plusDrd2 = nnz(validCells{1} & validCells{2} & validCells{3})
 
-%% Plotting
+%% Plot images for sanity checks
+%
+% These images show a merged image of the 520 and 650 channels. There is a
+% green outline for each GFP labeled cell. Cells that meet the threshold
+% criteria for puncta are marked with a red circle (puncta in 520 nm
+% channel) or a white cross (puncta in 650 nm channel).
 
-iC = 2;
+%Normalize the 520 and 650 data for better visualization
+ch520Norm = double(mip(:, :, 3));
+ch520Norm = (ch520Norm - min(ch520Norm(:)))/(max(ch520Norm(:)) - min(ch520Norm(:)));
 
-showoverlay(mip(:, :, iC + 1), bwperim(nuclMask), 'Opacity', 40)
+ch650Norm = double(mip(:, :, 4));
+ch650Norm = (ch650Norm - min(ch650Norm(:)))/(max(ch650Norm(:)) - min(ch650Norm(:)));
+
+%Create an RGB image of the 520 (yellow) and 650 (magenta) channels
+alphaBlend = 0.5;
+
+red = alphaBlend * ch520Norm + (1 - alphaBlend) * ch650Norm;
+green = alphaBlend * ch520Norm;
+blue = (1 - alphaBlend) * ch650Norm;
+
+rgbOut = cat(3, red, green, blue);
+
+%Overlay markers to identify cells with puncta
+figure(2)
+showoverlay(rgbOut, bwperim(nuclMask), 'Opacity', 40)
 hold on
-for ii = 1:numel(chCellData{iC})
-    if validCells{iC}(ii)
-        plot(chCellData{iC}(ii).Centroid(1), chCellData{iC}(ii).Centroid(2), 'ro')
+for iC = [2, 3]
+    for ii = 1:numel(chCellData{iC})
+        if validCells{iC}(ii)
+            if iC == 2
+            plot(chCellData{iC}(ii).Centroid(1), chCellData{iC}(ii).Centroid(2), 'ro')
+            else
+                plot(chCellData{iC}(ii).Centroid(1), chCellData{iC}(ii).Centroid(2), 'wx')
+            end
+        end
     end
 end
 hold off
